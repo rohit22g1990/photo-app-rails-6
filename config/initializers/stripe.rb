a6e6703357f574dc27839b3cac8e271fd5a6ff41
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-05-04 22:33:55 +0530

    Temporary change

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
-Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.stripe[:stripe_test_publishable_key],
-  :secret_key => Rails.application.credentials.stripe[:stripe_test_secret_key]
-}
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Rails.configuration.stripe = {
+#   :publishable_key => Rails.application.credentials.stripe[:stripe_test_publishable_key],
+#   :secret_key => Rails.application.credentials.stripe[:stripe_test_secret_key]
+# }
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]

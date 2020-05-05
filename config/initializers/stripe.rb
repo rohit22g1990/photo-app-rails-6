8b01ff9d8f785129f44c3b319220058c808e1d93
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-05-05 09:37:42 +0530

    Stripe syntax error

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,5 +8,6 @@ else
     :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
     :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
   }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]  
